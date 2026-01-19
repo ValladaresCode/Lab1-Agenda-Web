e3f72aaeb3dc44cbe52af1f1144d114525027cec
ValladaresCode <rvalladares-2024060@kinal.edu.gt>
2026-01-19 14:59:27 -0600

    C6. creacion de favoritos

---
+++ js/favoritos.js
@@ -0,0 +1,14 @@
+function verDetalle() {
+    window.location.href = "../detalle-contacto.html";
+}
+
+// Búsqueda (nombre o teléfono)
+document.getElementById("search").addEventListener("keyup", function () {
+    const filter = this.value.toLowerCase();
+    const rows = document.querySelectorAll("#contactsTable tr");
+
+    rows.forEach(row => {
+        const text = row.innerText.toLowerCase();
+        row.style.display = text.includes(filter) ? "" : "none";
+    });
+});
