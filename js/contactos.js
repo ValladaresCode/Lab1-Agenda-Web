97e53cbcb27216129473c0e618d6026a6658f9f0
ValladaresCode <rvalladares-2024060@kinal.edu.gt>
2026-01-19 14:47:40 -0600

    C4. creacion de contactos

---
+++ js/contactos.js
@@ -0,0 +1,20 @@
+const searchInput = document.getElementById("search");
+const rows = document.querySelectorAll("#contactsTable tr");
+
+searchInput.addEventListener("keyup", () => {
+    const value = searchInput.value.toLowerCase();
+
+    rows.forEach(row => {
+        const text = row.innerText.toLowerCase();
+        row.style.display = text.includes(value) ? "" : "none";
+    });
+});
+
+function verDetalle() {
+    window.location.href = "../detalle-contacto.html";
+}
+
+function logout() {
+    localStorage.clear();
+    window.location.href = "../index.html";
+}
